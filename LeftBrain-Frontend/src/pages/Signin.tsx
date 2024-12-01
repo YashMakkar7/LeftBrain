9d73f9b5f2ce77b407de29de0922504b78bd62f3
Yash Makkar <yashmakkar777@gmail.com>
2024-12-01 16:37:00 +0530

    conditional rendering based on status code is added in signup and signin page

--- LeftBrain-Frontend/src/pages/Signin.tsx
+++ LeftBrain-Frontend/src/pages/Signin.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
 import { BACKEND_URL } from "../config";
 import axios from "axios"
 import { useNavigate } from "react-router-dom";
 
-export function Signin(){
+export function Signin() {
     const usernameRef = useRef<HTMLInputElement>();
     const passwordRef = useRef<HTMLInputElement>();
     const navigate = useNavigate();
+    const [statusCode, setStatusCode] = useState(null)
 
     useEffect(() => {
         if (usernameRef.current) {
@@ -16,23 +17,37 @@ export function Signin(){
         }
     }, []);
 
-    async function Signin(){
+    async function Signin() {
         const username = usernameRef.current?.value;
         const password = passwordRef.current?.value;
-        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`,{
-            username,password
-        })
-        //@ts-ignore
-        const jwt= response.data.token;
-        localStorage.setItem("token",jwt)
-        navigate("/dashboard")
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+                username, password
+            })
+            //@ts-ignore
+            const jwt = response.data.token;
+            localStorage.setItem("token", jwt)
+            navigate("/dashboard")
+        } catch (e: any) {
+            setStatusCode(e.response.status)
+        }
+    }
+    async function forwardToSignup() {
+        navigate("/signup")
     }
     return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
-        <div className="bg-white border min-w-48 px-4 py-8 rounded-2xl">
-            <Input reference={usernameRef} placeholder="Username"/>
-            <Input reference={passwordRef} placeholder="Password"/>
-            <div className="justify-center flex pt-4 mx-8" onClick={Signin}>
-                <Button varient="primary" text="Signin" fullWidth={true} loading={false}/>
+        <div className="bg-white border min-w-48 px-4 py-6 rounded-2xl">
+            <Input reference={usernameRef} placeholder="Username" />
+            <Input reference={passwordRef} placeholder="Password" />
+
+            {statusCode == 404 && <div className="justify-center flex flex-pt-1 mx-8 max-w-72 text-red-600 text-sm font-semibold">User not exist</div>}
+            {statusCode == 411 && <div className="justify-center flex flex-pt-1 mx-8 max-w-72 text-red-600 text-sm font-semibold">Incorrect Credentials</div>}
+
+            <div className="justify-center flex pt-2 mx-8" onClick={Signin}>
+                <Button varient="primary" text="Signin" fullWidth={true} loading={false} />
+            </div>
+            <div className="justify-centre gap-1 flex  mx-10 max-w-72 text-grey-600 text-sm font-sans pt-2  ">
+                <div>Don't Have an account ?</div> <div><button onClick={forwardToSignup} className="text-purple-600"> Signup</button></div>
             </div>
         </div>
     </div>
--- LeftBrain-Frontend/src/pages/Signup.tsx
+++ LeftBrain-Frontend/src/pages/Signup.tsx
@@ -1,28 +1,45 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
 import { BACKEND_URL } from "../config";
 import axios from "axios"
 import { useNavigate } from "react-router-dom";
 
-export function Signup(){
+export function Signup() {
     const usernameRef = useRef<HTMLInputElement>();
     const passwordRef = useRef<HTMLInputElement>();
     const navigate = useNavigate();
-    async function Signup(){
+    const [statusCode, setStatusCode] = useState(null)
+    async function Signup() {
         const username = usernameRef.current?.value;
         const password = passwordRef.current?.value;
-        await axios.post(`${BACKEND_URL}/api/v1/signup`,{
-            username,password
-        })
+        try {
+            await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+                username, password
+            })
+            navigate("/")
+        } catch (e: any) {
+            setStatusCode(e.response.status)
+        }
+    }
+    async function forwardToSignin() {
         navigate("/")
     }
     return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
-        <div className="bg-white border min-w-48 p-8 rounded-xl">
-            <Input reference={usernameRef} placeholder="Username"/>
-            <Input reference={passwordRef} placeholder="Password"/>
-            <div className="justify-center flex pt-4">
-                <Button varient="primary" text="Signup" fullWidth={true} loading={false} onClick={Signup}/>
+        <div className="bg-white border min-w-48 px-4 py-6 rounded-2xl">
+            <Input reference={usernameRef} placeholder="Username" />
+            <Input reference={passwordRef} placeholder="Password" />
+
+            {statusCode == 400 && <div className="justify-center flex flex-pt-1 mx-8 max-w-72 text-red-600 text-sm font-semibold">Password should have minimum length of 4 atleast one uppercase, one lowercase, one special character, one number</div>}
+
+            {statusCode == 404 && <div className="justify-center flex flex-pt-1 mx-8 max-w-72 text-red-600 text-sm font-semibold">User Already Exists</div>}
+
+            <div className="justify-center flex pt-2 mx-8">
+                <Button varient="primary" text="Signup" fullWidth={true} loading={false} onClick={Signup} />
+            </div>
+
+            <div className="justify-centre gap-1 flex  mx-10 max-w-72 text-grey-600 text-sm font-sans pt-2  ">
+                <div>Already Have an account ?</div> <div><button onClick={forwardToSignin} className="text-purple-600"> Signin</button></div>
             </div>
         </div>
     </div>
